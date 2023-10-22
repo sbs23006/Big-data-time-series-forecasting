39ed40032fbda7ce4a87b5eb58614726fe90854c
Valentina Martucci <sbs32006@student.cct.ie>
2023-10-22 19:53:45 +0100

    clean

--- hadoop_jobs/filter_clean/reducer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-# Process each line from the input
-for line in sys.stdin:
-    # Print the line as it is (after removing leading/trailing whitespaces)
-    print(line.strip())
