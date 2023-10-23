d8b744403cbc9366f821d792b51d00524ebc9f43
Valentina Martucci <sbs32006@student.cct.ie>
2023-10-23 17:03:48 +0100

    preprocessing script updated

--- hadoop_jobs/filter_clean/mapper.py
+++ hadoop_jobs/filter_clean/mapper.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 import sys
 import csv
+import string
+import nltk
+from nltk.corpus import stopwords
+
+# Download the stopwords from NLTK
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
 
 reader = csv.reader(sys.stdin)
 
-for row in reader:
-    if row[1] and row[4]:  # assuming 'date' is the first column and 'text' is the fourth column
-        print('%s\t%s\t%s' % (row[1], row[3], row[4]))  # output 'date', 'user', and 'text'
+# remove empty rows with user and text 
+# remove not needed columns
+# remove punctuation from the text
+# remove stopwords
 
+for row in reader:
+    if row[1] and row[4]:
+        # Remove punctuation from 'text'
+        text = row[4].translate(str.maketrans('', '', string.punctuation))
+        # Remove stopwords
+        text = ' '.join(word for word in text.split() if word not in stop_words)
+        print('%s,%s,%s' % (row[1], row[3], text))  # output 'date', 'user', and 'text'
