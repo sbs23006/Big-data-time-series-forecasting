f711caa4efafcaceda6b35633b409ddfce1d8e11
Valentina Martucci <sbs32006@student.cct.ie>
2023-10-24 20:08:57 +0100

    processedData job + stored on Hbase

--- hadoop_jobs/filter_clean/reducer.py
+++ hadoop_jobs/filter_clean/reducer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import sys
 
+row_key = 1 #needed to write back to Hbase
+
 # Process each line from the input
 for line in sys.stdin:
-    # Print the line as it is (after removing leading/trailing whitespaces)
-    print(line.strip())
+    # Print the line as it is + id for hbase
+    print('{},{}'.format(row_key, line.strip()))
+    row_key += 1
